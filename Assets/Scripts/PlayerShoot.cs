9ca311ccadd00676f69a056481fb46387b2d6aca
Zattri <daninjakidy@gmail.com>
2019-01-01 22:59:49 +0000

    Added bullet and shooting mechanics

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Camera cam;
+    float bulletImpulse = 15f;
+
+    void Start()
+    {
+        cam = GetComponentInChildren<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, cam.transform.position + cam.transform.forward, cam.transform.rotation);
+            bullet.transform.Rotate(180, 0, 0);
+            Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+            bulletRb.AddForce(cam.transform.forward * bulletImpulse, ForceMode.Impulse);
+        }
+    }
+}
